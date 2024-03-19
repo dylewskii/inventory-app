bb09e8fd6ba205585a5b49f393fbb18be76e7ace
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-19 16:35:10 +0000

    add movieController to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const movieController = require("../controllers/movieController");
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "ClipFlix" });
-});
+router.get("/", movieController.index);
 
 module.exports = router;
